10a8bbce3bafe288045163ee4d96a4b6fcdf9683
JasonAeon <83183462+JasonAeon@users.noreply.github.com>
2021-04-27 08:56:23 +0200

    Site setup

---
+++ aeon-site/src/App.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let name: string = 'ÆON';
+</script>
+
+<main>
+	<h1>{name.toUpperCase()}</h1>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 0.1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ffa900;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
